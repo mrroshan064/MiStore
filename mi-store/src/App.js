59d897692767c6e819c57f94190151dd0b1ca09b
mrroshan064 <roshanmahato351@gmil.com>
2022-11-28 17:23:38 +0545

    updated

--- mi-store/src/App.js
+++ mi-store/src/App.js
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import { BrowserRouter } from "react-router-dom";
 import Slider from "./components/Slider.js";
 import data from "./data/data.json";
-import Offer from "./components/Offer.js"
+import Offer from "./components/Offer.js";
 
 const App = () => {
   return (
@@ -12,7 +12,7 @@ const App = () => {
       <PreNavbar />
       <Navbar logo={data.logo} />
       <Slider start={data.banner.start} />
-      <Offer/>
+      <Offer />
     </BrowserRouter>
   );
 };
